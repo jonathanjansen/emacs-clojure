563d75b64f1a4492b0bc00ff5c461ebb35ddbd05
Jonathan Jansen <jj@leadtechnologist.com>
2019-12-02 10:27:50 +0200

    init and clojure cider setup

---
+++ init.el
@@ -0,0 +1,60 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages
+  '(cider
+    clojure-mode
+    ido-completing-read+
+    projectile
+    rainbow-delimiters
+    tagedit
+    smex
+    magit)
+  )
+
+(if (eq system-type 'darwin)
+    (add-to-list 'my-packages 'exec-path-from-shell))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-to-list 'load-path "~/.emacs.d/customizations")
+
+(load "shell-integration.el")
+
+(load "editing.el")
+
+(load "navigation.el")
+
+(load "setup-clojure.el")
+
+(load "setup-js.el")
+
+(load "ui.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit tagedit rainbow-delimiters projectile ido-completing-read+ cider exec-path-from-shelL smex exec-path-from-shell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
